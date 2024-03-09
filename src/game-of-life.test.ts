5f4c1063b1a0c385e7930917a4ebc8ef2653f1d5
Pelle Lundgren <pellburt@gmail.com>
2024-03-09 15:14:38 +0100

    Add rules in plain text, rename jest config to js from ts to get rid of red tsconfig file

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -1,3 +1,13 @@
+/**
+ * Rules
+ * -------------------
+ * a living cell with fewer than 2 neighbors dies,
+ * a living cell with 2 living neighbors survives,
+ * a living cell with 3 living neighbors survives,
+ * a living cell with more than 3 living neighbors dies,
+ * a dead cell with 3 neighbors alive comes to life.
+ */
+
 import { sum } from "./game-of-life";
 
 test("adds 1 + 2 to equal 3", () => {
