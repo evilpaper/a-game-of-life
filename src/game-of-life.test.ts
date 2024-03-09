ecb8f20412690a87674d01558ab7a8bbaa1f7e02
Pelle Lundgren <pellburt@gmail.com>
2024-03-09 16:29:26 +0100

    Add board class

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -8,8 +8,21 @@
  * a dead cell with 3 neighbors alive comes to life.
  */
 
-import { sum } from "./game-of-life";
+// import { sum } from "./game-of-life";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+import { Board } from "./game-of-life";
+
+const boardOfDeads: number[][] = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+it("returns empty board when all given cells are dead", () => {
+  const board = new Board(boardOfDeads);
+  expect(board.tick()).toEqual(boardOfDeads);
 });
--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,3 +1,15 @@
-export function sum(a: number, b: number): number {
-  return a + b;
+// export function sum(a: number, b: number): number {
+//   return a + b;
+// }
+
+export class Board {
+  constructor(private board: number[][]) {}
+
+  public tick() {
+    return [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+  }
 }
