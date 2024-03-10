2c2ada810982f452d2c7f8a875f3b5cf0931caad
Pelle Lundgren <pellburt@gmail.com>
2024-03-10 16:21:45 +0100

    Complete tests for main test cases for board and cell

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -1,3 +1,7 @@
+import { Board } from "./game-of-life";
+import { Cell } from "./game-of-life";
+import { it } from "@jest/globals";
+
 /**
  * Rules
  * -------------------
@@ -6,23 +10,83 @@
  * a living cell with 3 living neighbors survives,
  * a living cell with more than 3 living neighbors dies,
  * a dead cell with 3 neighbors alive comes to life.
+ *
+ *
+ * Test cases
+ * ------------------
+ * - any cell with fewer than 2 neighbors, regardless of whether it is currently alive or dead, will die after a tick,
+ * - every cell with exactly 3 neighbors, regardless of whether it is currently alive or dead, will be alive after the "tick",
+ * - if a cell currently has 2 neighbors, the cell will only be alive after tick if it’s already alive.
  */
 
-// import { sum } from "./game-of-life";
+describe("Game Of Life", () => {
+  describe("Cell", () => {
+    it.each([new Cell(1, 1), new Cell(1, 0), new Cell(0, 1), new Cell(0, 0)])(
+      'cell will be dead after tick when has less than 2 neighbors"',
+      (cell) => {
+        expect(cell.setNextState()).toEqual(0);
+      }
+    );
 
-// test("adds 1 + 2 to equal 3", () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+    it("cell will be alive after tick when has 3 neighbors", () => {
+      const cell = new Cell(1, 3);
+      expect(cell.setNextState()).toEqual(1);
+    });
 
-import { Board } from "./game-of-life";
+    it("alive cell will be alive after tick when has 2 neighbors", () => {
+      const cell = new Cell(1, 2);
+      expect(cell.setNextState()).toEqual(1);
+    });
+  });
+  describe("Board", () => {
+    describe("returns proper board after tick", () => {
+      it("sample board 1", () => {
+        const board = {
+          before: [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+          ],
+          after: [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+          ],
+        };
+        expect(new Board(board.before).tick()).toEqual(board.after);
+      });
 
-const boardOfDeads: number[][] = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
+      it("sample board 2", () => {
+        const board = {
+          before: [
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1],
+          ],
+          after: [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0],
+          ],
+        };
+        expect(new Board(board.before).tick()).toEqual(board.after);
+      });
 
-it("returns empty board when all given cells are dead", () => {
-  const board = new Board(boardOfDeads);
-  expect(board.tick()).toEqual(boardOfDeads);
+      it("sample board 3", () => {
+        const board = {
+          before: [
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 1, 0],
+          ],
+          after: [
+            [0, 0, 0],
+            [1, 0, 1],
+            [1, 1, 0],
+          ],
+        };
+        expect(new Board(board.before).tick()).toEqual(board.after);
+      });
+    });
+  });
 });
--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,15 +1,47 @@
-// export function sum(a: number, b: number): number {
-//   return a + b;
-// }
-
 export class Board {
   constructor(private board: number[][]) {}
 
   public tick() {
-    return [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ];
+    this.board = this.board.map((row: number[], rowIdx: number) => {
+      return row.map((__, cellIdx: number) => {
+        const neighbors = [
+          this.getCellContent(cellIdx - 1, rowIdx - 1),
+          this.getCellContent(cellIdx - 1, rowIdx),
+          this.getCellContent(cellIdx - 1, rowIdx + 1),
+          this.getCellContent(cellIdx, rowIdx - 1),
+          this.getCellContent(cellIdx, rowIdx + 1),
+          this.getCellContent(cellIdx + 1, rowIdx - 1),
+          this.getCellContent(cellIdx + 1, rowIdx),
+          this.getCellContent(cellIdx + 1, rowIdx + 1),
+        ];
+        const neighborsSum = neighbors.reduce((sum, a) => sum + a, 0);
+        const cell = new Cell(
+          this.getCellContent(rowIdx, cellIdx),
+          neighborsSum
+        );
+        return cell.setNextState();
+      });
+    });
+    return this.board;
+  }
+
+  private getCellContent(cellIndex: number, rowIndex: number) {
+    return this.board[rowIndex] && this.board[rowIndex][cellIndex] ? 1 : 0;
+  }
+}
+export class Cell {
+  constructor(private state: number, private neighborsSum: number) {}
+
+  public setNextState(): number {
+    if (this.hasThreeNeighbors() || this.hasTwoNeighborsAndAlive()) return 1;
+    return 0;
+  }
+
+  public hasThreeNeighbors(): boolean {
+    return this.neighborsSum === 3;
+  }
+
+  public hasTwoNeighborsAndAlive(): boolean {
+    return this.state === 1 && this.neighborsSum === 2;
   }
 }
